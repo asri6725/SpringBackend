c29b3e2b0b1bc525e75e0a7b26da4bc0910b1854
Abhi <abhinandan.srinivas@gmail.com>
2020-08-25 02:13:03 +1000

    Json Api created

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,30 @@
+
+package com.example.demo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import java.util.*; 
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+  
+    
+    public static void main(String[] args) {
+    SpringApplication.run(DemoApplication.class, args);
+    }
+    
+    @GetMapping("/")
+    public List hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        List<Integer> list = new ArrayList<Integer>() {{ 
+            add(1); 
+            add(3); 
+            } }; 
+    return list;
+    }
+  
+}
+            
