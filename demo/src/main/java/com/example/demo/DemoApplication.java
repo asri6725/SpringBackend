839e15437a4c3efbfd05d9882726af2b742a0abf
Abhi <abhinandan.srinivas@gmail.com>
2020-08-25 15:06:09 +1000

    Front end and api

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,7 +2,7 @@
 package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.stereotype.Controller;
@@ -10,6 +10,7 @@ import java.util.*;
 
 @SpringBootApplication
 @RestController
+@CrossOrigin("*")
 public class DemoApplication {
   
     
